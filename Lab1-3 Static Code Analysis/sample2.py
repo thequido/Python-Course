891238146a9d3b4844ccf70729560ee991449a0b
Joe Guido <joeguido41988@gmail.com>
2023-02-22 21:05:00 -0600

    Update sample2.py

--- Lab1-3 Static Code Analysis/sample2.py
+++ Lab1-3 Static Code Analysis/sample2.py
@@ -9,4 +9,4 @@ def add(number1, number2):
 NUM1 = 4
 NUM2 = 5
 TOTAL = add(NUM1, NUM2)
-print("The sum of {} and {} is {}".format(NUM1, NUM2, TOTAL))
+print(f"The sum of {NUM1} and {NUM2} is {TOTAL}")
