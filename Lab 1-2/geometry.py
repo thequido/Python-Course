babb590720fd674d4b28ca87fce007ac093725eb
Joe Guido <joeguido41988@gmail.com>
2023-02-22 19:13:29 -0600

    Add files via upload

---
+++ Lab 1-2/geometry.py
@@ -0,0 +1,12 @@
+#Code for new module below
+def area_of_rectangle(l,w):
+    """
+    This function returns the area of a rectangle
+    """
+    return l * w
+
+def area_of_circle(r):
+    """
+    This function returns the area of a circle
+    """
+    return 3.1415 * (r ** 2)
